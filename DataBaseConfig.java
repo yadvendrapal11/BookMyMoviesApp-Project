bc358ef80c94f05b2df07a738c1756b3d9ee81e2
yadvendra11 <yadvendrapal11@gmail.com>
2025-05-31 14:36:14 +0530

    added configuration file for BookMyMovies project

---
+++ DataBaseConfig.java
@@ -0,0 +1,17 @@
+package Configurartion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConfig {
+
+    static String url = "jdbc:mysql://localhost:3306/bookmymovies";
+    static String username = "root";
+    static String password = "Anoop@123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url , username , password);
+
+    }
+}
